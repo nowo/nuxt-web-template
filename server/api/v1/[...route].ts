6ddc694e36f5ef6817c2a5d70843c5bd0c7fc7a9
cooj <z8888788@163.com>
2024-07-30 10:52:50 +0800

    feat(utils): 增加留言表数据模型及对应的数据接口，其他信息接口

--- server/api/v1/[...route].ts
+++ server/api/v1/[...route].ts
@@ -5,6 +5,8 @@ import { getSystemInfo, setSystemInfo } from '~~/server/controller/system'
 import { getMenuList, setMenuCreate, setMenuDelete, setMenuUpdate } from '~~/server/controller/menu'
 import { getProductList, setProductCreate, setProductDelete, setProductUpdate } from '~~/server/controller/product'
 import { getModelList } from '~~/server/controller/model'
+import { getOtherInfo, setOtherUpdate } from '~~/server/controller/other'
+import { getMessageList, setMessageCreate, setMessageDelete, setMessageUpdate } from '~~/server/controller/message'
 
 const router = createRouter()
 
@@ -36,125 +38,14 @@ router.use('/product/add', setProductCreate) // 添加产品
 router.use('/product/edit', setProductUpdate) // 修改产品
 router.use('/product/delete', setProductDelete) // 删除产品
 
-/**
- * 获取所有菜单信息
- */
-router.use('/list', defineEventHandler(async (event) => {
-    const list = [
-        {
-            // id: 1,
-            name: 'home',
-            path: '/home',
-            // icon: 'home',
-            children: [
-                {
-                    id: 11,
-                    name: '首页',
-                    path: '/home',
-                    icon: 'home',
-                },
-            ],
-        },
-        {
-            path: '/system',
-            name: 'system',
-            meta: {
-                layout: 'classic',
-                title: '系统设置',
-            },
-            props: {
-                default: false,
-            },
-            children: [
-                {
-                    path: '/system/info',
-                    name: 'info',
-                    meta: {
-                        title: '系统信息',
-                    },
-                    props: {
-                        default: false,
-                    },
-                },
-            ],
-            instances: {},
-            leaveGuards: {},
-            updateGuards: {},
-            enterCallbacks: {},
-            components: {},
-        },
-    ]
+router.use('/other/info', getOtherInfo) // 获取其他信息
+router.use('/other/update', setOtherUpdate) // 修改其他信息
 
-    return list
-}))
+router.use('/message/list', getMessageList) // 留言列表
+router.use('/message/add', setMessageCreate) // 添加留言
+router.use('/message/edit', setMessageUpdate) // 修改留言
+router.use('/message/delete', setMessageDelete) // 删除留言
 
-/**
- * 获取用户权限菜单
- */
-router.use('/auth', defineEventHandler(async (event) => {
-    const list = [
-        {
-            // id: 1,
-            name: 'index',
-            path: '/',
-            // icon: 'home',
-            redirect: '/home',
-            meta: {
-                title: '首页',
-                icon: 'i-ep-home-filled',
-            },
-            children: [
-                {
-                    name: '首页',
-                    path: '/home',
-                    meta: {
-                        title: '首页',
-                        icon: 'i-ep-home-filled',
-                    },
-                },
-            ],
-        },
-        {
-            path: '/system',
-            name: 'system',
-            meta: {
-                layout: 'classic',
-                title: '系统设置',
-            },
-            props: {
-                default: false,
-            },
-            children: [
-                {
-                    path: '/system/info',
-                    name: 'info',
-                    meta: {
-                        title: '系统信息',
-                    },
-                    props: {
-                        default: false,
-                    },
-                },
-            ],
-            instances: {},
-            leaveGuards: {},
-            updateGuards: {},
-            enterCallbacks: {},
-            components: {},
-        },
-    ]
 
-    return list
-}))
-
-const startAt = Date.now()
-let count = 0
-
-router.use('/pageview', defineEventHandler(async (event) => {
-    return {
-        pageview: count++,
-        startAt,
-    }
-}))
 
 export default useBase('/api/v1', router.handler)
---
+++ server/controller/message.ts
@@ -0,0 +1,163 @@
+import type { Prisma } from '@prisma/client'
+
+
+// banner
+
+/**
+ * 列表查询
+ * @param event
+ * @returns
+ */
+export const getMessageList = defineEventHandler(async event => {
+    // 接口校验(是否登录)
+    // if (!event.context.user) return ResponseMessage.token
+
+    // 获取参数
+    const param = await getEventParams<{ status: number, title: string } & IListPage>(event)
+
+    // if (!param?.type) return { msg: '请传递类型' }
+
+    // const types = param?.type.toString().split(',').filter(item => !!item).map(item => Number(item))
+    const where: Prisma.MessageWhereInput = {
+        // type: { in: types },
+        // isHide: false,
+    }
+
+    if (param?.title) where.title = { contains: param.title }
+
+    // 状态，判断是否为数字
+    if (/^\d+$/.test(param?.status?.toString() || '')) where.status = Number(param?.status)
+
+    // 查询菜单姓"张"，1页显示20条
+    let page: number | undefined
+    let pageSize: number | undefined
+    let pageSkip: number | undefined
+
+    if (param?.isPage) {
+        page = Number(param.page || 1)
+        pageSize = Number(param.pageSize || 20)
+        pageSkip = pageSize * (page - 1) || 0
+    }
+
+    const [res1, res2] = await Promise.all([
+        prisma.message.findMany({
+            skip: pageSkip,
+            take: pageSize,
+            where,
+            orderBy: {
+                createdAt: 'desc', // 按id正序排序
+            },
+            // include: {
+            //     Product: {
+            //         include: {
+            //             links: {
+            //                 where: {
+            //                     type: 1,
+            //                 },
+            //             },
+            //         },
+            //     },
+            // },
+            // select: { // 只返回指定的字段
+            //     username: true,
+            //     account: true,
+            // },
+        }),
+        prisma.message.count({
+            where,
+        }),
+    ])
+    const list = res1
+    if (res1) {
+        return { code: 200, data: { list, total: res2 } }
+    } else {
+        return { code: 400, message: '查询失败' }
+    }
+})
+
+/**
+ * 新增
+ * @param event
+ * @returns
+ */
+export const setMessageCreate = defineEventHandler(async event => {
+    // const url = getRequestURL(event)
+    // // /api/page**的接口，跳过登录校验
+    // if (!url.pathname.includes('/api/page')) {
+    //     // 接口校验(是否登录)
+    //     if (!event.context.user) return ResponseMessage.token
+    // }
+    // 接口校验(是否登录)
+    // if (!event.context.user) return ResponseMessage.token
+
+    // 获取参数
+    const param = await getEventParams<Prisma.MessageCreateInput>(event)
+
+    if (!param?.title) return { msg: '名称不能为空' }
+
+    const res = await prisma.message.create({
+        data: param,
+    })
+
+    if (res) {
+        return { code: 200, msg: '添加成功' }
+    } else {
+        return { msg: '网络错误' }
+    }
+})
+
+/**
+ * 修改
+ * @param event
+ * @returns
+ */
+export const setMessageUpdate = defineEventHandler(async event => {
+    // 接口校验(是否登录)
+    // if (!event.context.user) return ResponseMessage.token
+
+    // 获取参数
+    const param = await getEventParams<{ id: number } & Prisma.MessageUpdateInput>(event)
+
+    if (!param?.id) return { msg: '缺少参数id' }
+    if (!param?.title) return { msg: '标题不能为空' }
+
+    const res = await prisma.message.update({
+        data: param,
+        where: {
+            id: param.id,
+        },
+    })
+
+    if (res) {
+        return { code: 200, msg: '修改成功' }
+    } else {
+        return { msg: '网络错误' }
+    }
+})
+
+/**
+ * 删除
+ * @param event
+ * @returns
+ */
+export const setMessageDelete = defineEventHandler(async event => {
+    // 接口校验(是否登录)
+    // if (!event.context.user) return ResponseMessage.token
+
+    // 获取参数
+    const param = await getEventParams<{ id: number }>(event)
+
+    if (!param?.id) return { msg: '缺少参数id' }
+
+    const res = await prisma.message.delete({
+        where: {
+            id: param.id,
+        },
+    })
+
+    if (res) {
+        return { code: 200, msg: '删除成功' }
+    } else {
+        return { msg: '网络错误' }
+    }
+})
